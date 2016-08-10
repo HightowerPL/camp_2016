3b8ac3b363c3581c5a2775ed4d7de372c41d9d39
Jakub Snopkiewicz <Jakub Snopkiewicz>
2016-08-10 16:55:01 +0200

    EfiCamp zajecia 3

---
+++ js/button.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function(){
+    $('.button').on('click', function(event) {
+    let password =$('#password').val();
+    sendAjax(password);
+  });
+
+  function sendAjax(pass){
+    $.ajax({
+      type: "post",
+      data: {
+        login: "efi",
+        password: "camp"
+      },
+      url: "https://efigence-camp.herokuapp.com/api/login",
+      error: function(response) {
+        console.log(response.responseText);
+      },
+      success: function(response) {
+        console.log(response);
+      }
+    });
+  };
+  /*
+  var button = document.getElementsById("button");
+  var input = document.getElementsById("input_pass");
+
+  $(button).on('click', function (event){
+    event.preventDefault();
+    alert($(input).val());
+  });*/
+});
