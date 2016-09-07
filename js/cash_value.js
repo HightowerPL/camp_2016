bcae96b8ddf36a1dc8f80cca59ee987fa786aae2
Jakub Snopkiewicz <Jakub Snopkiewicz>
2016-09-07 02:29:42 +0200

    przed 7-mymi zajeciami

--- js/cash_value.js
+++ js/cash_value.js
@@ -6,12 +6,16 @@ function getAjax(){
     url: "https://efigence-camp.herokuapp.com/api/data/summary",
 
     success: function(response){
-      var fundusze = response.content[0].funds;
-      var balans = response.content[0].balance;
-      var platnosc = response.content[0].payments;
-      $(".livebal").html("<b>"+balans+"</b> PLN");
-      $(".livefun").html("<b>"+fundusze+"</b> PLN");
-      $(".livepay").html("<b>"+platnosc+"</b> PLN");
+      var fund = response.content[0].funds;
+      var bal = response.content[0].balance;
+      var pay = response.content[0].payments;
+      var funCcy = fund.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1 ");
+      var balCcy = bal.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1 ");
+      var payCcy = pay.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1 ");
+      
+      $(".livebal").html("<b>"+balCcy+"</b> PLN");
+      $(".livefun").html("<b>"+funCcy+"</b> PLN");
+      $(".livepay").html("<b>"+payCcy+"</b> PLN");
     }
   });
 }
--- js/history.js
+++ js/history.js
@@ -1,25 +1,31 @@
 $(document).foundation();
 
-function getAjax(){
+function ajaxPost( data, endpoint, method, success ) {
+
   $.ajax({
-    type:"GET",
-    url: "https://efigence-camp.herokuapp.com/api/data/history",
-
-    success: function(response){
-      console.log(response);
-
-      for (var i = 0; i < response.content.length ; i++){
-        var html = '<li>' + response.content[i].currency +' <li>';
-
-        $(".listaHis").append(html);
-      }
-    }
-  });
+    method: method,
+    url: "http://efigence-camp.herokuapp.com/api/"+endpoint,
+    data: data
+  })
+    .done(function( msg ) {
+      success(msg);
+    })
 }
+ajaxPost({}, "/data/history", "GET", function(response) {
+  $('.dateTrn').html(response.content[0].date);
+  $('.descTrn').html(response.content[0].description);
+  $('.valueTrn').html(response.content[0].amount + ' PLN');
 
-getAjax();
-/*
-$(".livebal").html("<b>"+balans+"</b> PLN");
-$(".livefun").html("<b>"+fundusze+"</b> PLN");
-$(".livepay").html("<b>"+platnosc+"</b> PLN");
-*/
+  for(var i=0; i < response.content.length; i++) {
+    console.log(response.content[i]);
+
+
+    var html = '<li class="row"><div class="medium-2 columns"><span class="dateTrn">'
+    + response.content[i].date +'</span></div><div class="medium-6 columns"><span class="descTrn">'
+    +  response.content[i].description +'</span></div><div class="medium-4 columns"><span class="valueTrn"><strong>'
+    + response.content[i].amount  + '</strong> PLN</span></div></li>';
+    $('.listHis').append(html);
+
+  }
+});
+ajaxPost();
